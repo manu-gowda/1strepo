afb828d868d8d68ec5e85a8bd76d024ee3ececb4
unknown <manu.xworkz@gmail.com>
2022-09-13 10:53:02 +0530

    Forest files

---
+++ Forest.java
@@ -0,0 +1,8 @@
+class Forest{
+	
+	static String name="bannergattaforest";
+	static String typeofforest="rainforest";
+	static String region="hassan";
+	static int totalarea=4635;
+	static String[] primaryanimals={"snake","dogs","elephant","bulls"};
+}
---
+++ ForestRunner.java
@@ -0,0 +1,15 @@
+class ForestRunner{
+	
+	public static void main (String[] sharath){
+		System.out.println(Forest.name);
+		System.out.println(Forest.typeofforest);
+		System.out.println(Forest.region);
+		System.out.println(Forest.totalarea);
+		System.out.println(Forest.primaryanimals);
+		for (int index=0; index<Forest.primaryanimals.length; index++)
+			{
+				String ref0=Forest.primaryanimals[index];
+				System.out.println("animals name:"+index+"name"+ref0);
+			}
+	}
+}
