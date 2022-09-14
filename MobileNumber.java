28c8410703c2c27699ac26d6b77b6e3d8b1b9533
unknown <manu.xworkz@gmail.com>
2022-09-14 22:43:01 +0530

    CapitalByState files

---
+++ MobileNumber.java
@@ -0,0 +1,60 @@
+class MobileNumber{
+	
+	static String getNameByNumber(long number)
+	{
+		System.out.println("Entered get NameByNumber");
+		if(number==9845306163L)
+		{
+			System.out.println("Manu Number is 9845306163L");
+			return "Manu";
+		}
+		if(number==8565646546L)
+		{
+			System.out.println("shinchu Number is 8565646546L");
+			return "shinchu";
+		}
+		if(number==9567565655L)
+		{
+			System.out.println("priya Number is 9567565655L");
+			return "priya";
+		}
+		if(number==9455465655L)
+		{
+			System.out.println("jjjjjjj Number is 9455465655L");
+			return "jjjjjjj";
+		}
+		if(number==8425635378L)
+		{
+			System.out.println("kruthi Number is 8425635378L");
+			return "kruthi";
+		}
+		if(number==9565363554L)
+		{
+			System.out.println("ammu Number is 9565363554L");
+			return "ammu";
+		}
+		if(number==9656657555L)
+		{
+			System.out.println("veena Number is 9656657555L");
+			return "veena";
+		}
+		if(number==9465757564L)
+		{
+			System.out.println("Manoj Number is 9465757564L");
+			return "Manoj";
+		}
+		if(number==9945789966L)
+		{
+			System.out.println("varsha Number is 9945789966L");
+			return "varsha";
+		}
+		if(number==81256878898L)
+		{
+			System.out.println("Yashu Number is 81256878898L");
+			return "Yashu";
+		}
+		return null;
+		
+	}
+	
+}
---
+++ MobileNumberRunner.java
@@ -0,0 +1,10 @@
+class MobileNumberRunner{
+	
+	public static void main(String[] mobile)
+	{
+		String ref=MobileNumber.getNameByNumber(9845306163L);
+		System.out.println(ref);
+		
+		
+	}
+}
